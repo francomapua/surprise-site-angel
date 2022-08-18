33eab34d4d27eeea2060ded98199be5c073d7507
Franco Mapua <fmapua@gmail.com>
2022-08-18 17:53:51 +0800

    Completing Slides Format

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,13 @@ var isPlaying = false
 
 function initialize(){
   // bgm.play()
+  document.getElementById('catslide2').addEventListener('click', () => showModal('modal-slide2'))
+  document.getElementById('modal-letter-close-slide-2').addEventListener('click', () => hideModal('modal-slide2'))
+
+  document.getElementById('catslide3').addEventListener('click', () => showModal('modal-slide3'))
+  document.getElementById('modal-gallery-close').addEventListener('click', () => hideModal('modal-slide3'))
+
+  document.getElementById('catslide4').addEventListener('click', () => showModal('modal-slide4'))
 }
 
 function bodyMouseHandler(){
@@ -16,3 +23,22 @@ function scrollToSection(sectionId){
   document.getElementById(sectionId).scrollIntoView({ block: 'end',  behavior: 'smooth' });
   // document.getElementById(sectionId).scrollIntoView({behavior: "smooth"})
 }  
+
+function showModal(modalId){
+  const modal = document.getElementById(modalId);
+  modal.style.display = "flex";
+}
+
+function hideModal(modalId){
+  const modal = document.getElementById(modalId);
+  modal.style.display = "none";
+}
+
+function sheSaidYes(){
+  hideModal('modal-slide4')
+  scrollToSection('slide5')
+}
+
+function sheSaidNo(){
+  alert('Well shit then')
+}
